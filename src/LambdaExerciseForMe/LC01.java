590b9e9f38c4de109640db6ae778b70c3e2a9bfc
Veysel2335 <Veyseldurmus92@gmail.com>
2022-05-06 16:58:08 +0200

    06.05.2022

---
+++ src/LambdaExerciseForMe/LC01.java
@@ -0,0 +1,15 @@
+package LambdaExerciseForMe;
+
+import java.util.List;
+
+public class LC01 {
+    public static void main(String[] args) {
+
+    }
+    //Create a meth to print list elemnts on the console in the same line with a space between two consecutive elements
+    public static void printElementsFunctional(List<Integer> list){
+        System system = null;
+        list.stream().forEach(system.out::print);
+
+    }
+}
